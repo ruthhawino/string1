5274a7c9030996d3e9523d04179e2cf22f4a1bf4
ruthhawino <awinoruthh@gmail.com>
2023-03-14 12:06:38 +0300

    first commit

---
+++ strings.js
@@ -0,0 +1,30 @@
+let b = "beatiful";
+// console.log(identity);
+console.log(b.length);
+console.log(b[5]);
+console.log(b[6]);
+console.log(b[7]);
+console.log(b[8]);
+const meal = "Iwas feeling hungry today";
+const mystring ="eat";
+console.log(meal[4]);
+const result = "I was ${mystring} feeling hungry today";
+console.log(result);
+
+const story="the quick brown fox jumps over the lazy dog";
+console.log(story.match(/the/g).length);
+console.log(story.match(/brown/g).length);
+const string5 ="we are now going to school";
+console.log(string5.includes("now"));
+console.log(strings.includes("sitting"));
+
+let s="wonderful";
+console.log(s.toUpperCase())
+let v="amazing";
+let t="BEneath";
+console.log(v.toLocaleLowerCase());
+console.log(t.toLocaleLowerCase());
+
+let d="A beautiful wedding";
+// d=d.split("").map([first,...rest])=>firstchar.toUpperCase()+rest.join("");
+console.log(d);
